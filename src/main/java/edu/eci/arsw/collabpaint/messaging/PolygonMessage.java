7dbc6beea2dcc530f4c178653d2eaeacf87b9d7c
unknown <santiago.rocha-c@labinfo.is.escuelaing.edu.co>
2023-11-02 16:00:32 -0500

    Arreglosy tests parte 4

--- src/main/java/edu/eci/arsw/collabpaint/messaging/PolygonMessage.java
+++ src/main/java/edu/eci/arsw/collabpaint/messaging/PolygonMessage.java
@@ -13,26 +13,28 @@ import org.springframework.stereotype.Controller;
 
 import edu.eci.arsw.collabpaint.model.Point;
 import java.awt.Polygon;
+
 @Controller
 public class PolygonMessage {
     @Autowired
     SimpMessagingTemplate smt;
-    private Map<String, List<Point>> pointsMap = new ConcurrentHashMap<>();
-     
-     @MessageMapping("/app/newpoint.{numdibujo}")
-     public void handleNewPoint(Point pt, @DestinationVariable String numdibujo) throws Exception{
+    
+    private static Map<String, List<Point>> pointsMap = new ConcurrentHashMap<>();
+
+    @MessageMapping("/app/newpoint.{numdibujo}")
+    public void handleNewPoint(Point pt, @DestinationVariable String numdibujo) throws Exception {
         System.out.println("Nuevo punto recibido en el servidor!:" + pt);
         List<Point> points = pointsMap.computeIfAbsent(numdibujo, k -> new ArrayList<>());
         points.add(pt);
 
-        if(points.size() >= 3){
+        if (points.size() >= 3) {
             Polygon polygon = createPolygonFromPoints(points);
             smt.convertAndSend("/topic/newpolygon." + numdibujo, polygon);
         }
 
-        
-     }
-     private Polygon createPolygonFromPoints(List<Point> points) {
+    }
+
+    private Polygon createPolygonFromPoints(List<Point> points) {
 
         int[] xPoints = new int[points.size()];
         int[] yPoints = new int[points.size()];
@@ -44,5 +46,5 @@ public class PolygonMessage {
 
         return new Polygon(xPoints, yPoints, points.size());
     }
-    
+
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,136 +1,136 @@
 var app = (function () {
     class Point {
-      constructor(x, y) {
-        this.x = x;
-        this.y = y;
-      }
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
     }
-  
+
     var stompClient = null;
     var theMessage = null;
     var currentNumber = null;
-  
+    var pointsBuffer = [];
+    var mateo = [];
+
     var addPointToCanvas = function (point) {
-      var canvas = document.getElementById("canvas");
-      var ctx = canvas.getContext("2d");
-      ctx.beginPath();
-      ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
-      ctx.stroke();
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
+        ctx.stroke();
+        mateo.push(point)
     };
-  
+
     function captureClickEvent() {
-      var canvas = document.getElementById("canvas");
-  
-      canvas.addEventListener("click", function (event) {
-        if (canvas.getAttribute("data-selected-canvas") === "true") {
-          var x = event.clientX - canvas.getBoundingClientRect().left;
-          var y = event.clientY - canvas.getBoundingClientRect().top;
-          publishPoint(x, y);
-        }
-      });
+        var canvas = document.getElementById("canvas");
+
+        canvas.addEventListener("click", function (event) {
+            if (canvas.getAttribute("data-selected-canvas") === "true") {
+                var x = event.clientX - canvas.getBoundingClientRect().left;
+                var y = event.clientY - canvas.getBoundingClientRect().top;
+                publishPoint(x, y);
+            }
+        });
     }
-  
+
     var getMousePosition = function (evt) {
-      canvas = document.getElementById("canvas");
-      var rect = canvas.getBoundingClientRect();
-      return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top,
-      };
+        canvas = document.getElementById("canvas");
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top,
+        };
     };
-  
+
     // Conectar y suscribirse a los canales de WebSocket
     var connectAndSubscribe = function (number) {
-      currentNumber = number;
-      console.info("Connecting to WS...");
-      var socket = new SockJS("/stompendpoint");
-      stompClient = Stomp.over(socket);
-      
-      stompClient.connect({}, function (frame) {
-        console.log("Connected: " + frame);
-  
-        // Suscribirse a /topic/newpoint
-        stompClient.subscribe("/topic/newpoint." + number, function (eventbody) {
-          theMessage = JSON.parse(eventbody.body);
-          addPointToCanvas(theMessage);
+        currentNumber = number;
+        console.info("Connecting to WS...");
+        var socket = new SockJS("/stompendpoint");
+        stompClient = Stomp.over(socket);
+
+        stompClient.connect({}, function (frame) {
+            console.log("Connected: " + frame);
+
+            // Suscribirse a /topic/newpoint
+            stompClient.subscribe("/topic/newpoint." + number, function (eventbody) {
+                theMessage = JSON.parse(eventbody.body);
+                addPointToCanvas(theMessage);
+            });
+
+            // Suscribirse a /topic/newpolygon
+            stompClient.subscribe("/topic/newpolygon." + number, function (eventbody) {
+                theMessage = JSON.parse(eventbody.body);    
+                drawPolygon(mateo);
+            });
         });
-  
-        // Suscribirse a /topic/newpolygon
-        stompClient.subscribe("/topic/newpolygon." + number, function (eventbody) {
-          theMessage = JSON.parse(eventbody.body);
-          console.log("Poligono Recibido");
-          drawPolygon(context, theMessage);
-        });
-      });
     };
-  
-    function drawPolygon(ctx, points) {
-      // Lógica para dibujar el polígono
-      // ...
-  
-      ctx.beginPath();
-      ctx.moveTo(points[0].x, points[0].y);
-  
-      for (var i = 1; i < points.length; i++) {
-        ctx.lineTo(points[i].x, points[i].y);
-      }
-  
-      ctx.lineTo(points[0].x, points[0].y);
-  
-      ctx.closePath();
-      ctx.fillStyle = "blue";
-      ctx.fill();
-      ctx.strokeStyle = "black";
-      ctx.lineWidth = 2;
-      ctx.stroke();
-    }
-  
-    var pointsBuffer = [];
-  
-    var publishPoint = function (px, py) {
-      var pt = new Point(px, py);
-      console.info("publishing point at " + pt);
-      addPointToCanvas(pt);
-  
-      pointsBuffer.push(pt);
-      if (pointsBuffer.length >= 3) {
-        stompClient.send("/app/newpolygon." + currentNumber, {}, JSON.stringify(pointsBuffer));
-      } else {
-        stompClient.send("/app/newpoint." + currentNumber, {}, JSON.stringify(pt));
-      }
-    };
-  
-    return {
-      init: function () {
-        var can = document.getElementById("canvas");
-  
-        document.getElementById("Send point").addEventListener("click", function () {
-          x = document.getElementById("x").value;
-          y = document.getElementById("y").value;
-          publishPoint(x, y);
-        });
-  
-        document.getElementById("Conectarse").addEventListener("click", function () {
-          number = document.getElementById("number").value;
-          var canvas = document.getElementById("canvas");
-          var contexto = canvas.getContext("2d");
-          contexto.clearRect(0, 0, canvas.width, canvas.height);
-          connectAndSubscribe(number);
-        });
-  
-        canvas.setAttribute("data-selected-canvas", "false");
-        canvas.addEventListener("click", function () {
-          canvas.setAttribute("data-selected-canvas", "true");
-        });
-        captureClickEvent();
-      },
-      disconnect: function () {
-        if (stompClient !== null) {
-          stompClient.disconnect();
+
+    function drawPolygon(points) {
+        let c2 = canvas.getContext('2d');
+        c2.fillStyle = '#ffa4a4';
+        c2.beginPath();
+        c2.moveTo(points[0].x, points[0].y);
+        for (let i = 1; i < points.length; i++) {
+            console.log(points[i]);
+            c2.lineTo(points[i].x, points[i].y);
         }
-        setConnected(false);
-        console.log("Disconnected");
-      },
+        c2.closePath();
+        c2.fill();
+    }
+
+    var publishPoint = function (px, py) {
+        var pt = new Point(px, py);
+        console.info("publishing point at " + pt);
+        addPointToCanvas(pt);
+
+        console.log("Este es el buffer")
+        console.log(pointsBuffer)
+        console.log("este es el codigo bien hecho de Mateo")
+        console.log(mateo)
+
+        pointsBuffer.push(pt);
+        if (pointsBuffer.length >= 3) {
+            stompClient.send("/topic/newpolygon." + currentNumber, {}, JSON.stringify(pointsBuffer));
+        } else {
+            stompClient.send("/topic/newpoint." + currentNumber, {}, JSON.stringify(pt));
+        }
     };
-  })();
-  
+
+    return {
+        init: function () {
+            var can = document.getElementById("canvas");
+
+            document.getElementById("Send point").addEventListener("click", function () {
+                x = document.getElementById("x").value;
+                y = document.getElementById("y").value;
+                publishPoint(x, y);
+            });
+
+            document.getElementById("Conectarse").addEventListener("click", function () {
+
+                
+                pointsBuffer = [];
+
+                number = document.getElementById("number").value;
+                var canvas = document.getElementById("canvas");
+                var contexto = canvas.getContext("2d");
+                contexto.clearRect(0, 0, canvas.width, canvas.height);
+                connectAndSubscribe(number);
+            });
+
+            canvas.setAttribute("data-selected-canvas", "false");
+            canvas.addEventListener("click", function () {
+                canvas.setAttribute("data-selected-canvas", "true");
+            });
+            captureClickEvent();
+        },
+        disconnect: function () {
+            if (stompClient !== null) {
+                stompClient.disconnect();
+            }
+            setConnected(false);
+            console.log("Disconnected");
+        },
+    };
+})();
